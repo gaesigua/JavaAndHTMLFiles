183c5ebfb28d94ac859fe44cfc1c4c99153f38fa
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-01-23 22:31:29 +0200

    jan23

---
+++ src/javaAndHTMLFiles/JavaAndHtmlFiles.java
@@ -0,0 +1,13 @@
+package javaAndHTMLFiles;
+
+public class JavaAndHtmlFiles {
+	public static void main(String[] args) {
+		
+//		1. I will ask the user for a file name
+//		2. Then check if file exists
+//		3. Determine if file is empty
+//		4. Rename .txt as .html
+//		5. I will read each line and insert necessary html <tags>
+	}
+
+}
