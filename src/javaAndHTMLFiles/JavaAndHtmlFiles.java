f20e61b8a7559e90e672ed344ecf1cbfa72ccd73
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-01-24 22:59:31 +0200

    jan24

--- src/javaAndHTMLFiles/JavaAndHtmlFiles.java
+++ src/javaAndHTMLFiles/JavaAndHtmlFiles.java
@@ -3,59 +3,140 @@ package javaAndHTMLFiles;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.PrintWriter;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class JavaAndHtmlFiles {
 	public static void main(String[] args) {
 		
-//1. Let's first create an empty file and call it "My365CodingJourney"
+//1. First of all, let's create an empty file and call it "My365CodingJourney"
 		
 //2. First we need a scanner to be able to ask for the file name from the user; and let's also create other variables we will need
 		
-		Scanner myScanner = new Scanner (System.in);	
-		Scanner fileIn;   // This will serve as our input file connection
-		PrintWriter fileOut; //This will serve as our HTML file connection
-		String fileNameIn; //This will be the original's file name
-		String fileNameOut; //This will be the new html's file name
-		int dotIndex; // This will help us locate (the position) where the dot is in the file name
-		String line = null; //Now we need a string to hold the current line/ basically it's a line from the input file
+		Scanner myScanner = new Scanner(System.in);
 		
+		Scanner fileAvailableInTheDirectory;
+		
+		PrintWriter filePrintedOut;
+		
+		String fileNameEnteredByUser;
+		String fileNameOut;
+		
+		int dotIndex;  //this is the position of the dot. in the filename
+		
+		String line = null; //this is a line from the input file
+		
+			
 //3. Now let's ask the user for the file name or the file path
-		System.out.println("Please Enter File name or File path");
-		fileNameIn= myScanner.nextLine();
 		
-//4. Now let's check if this file we just received exists
-//5. Let's Rename the file from .txt as .html file and we do that in the try block as well.
+		System.out.println("Please enter the file name or file path: ");
+		
+		fileNameEnteredByUser = myScanner.nextLine();	
+				
+	try {
 
+//4. Now let's check if this file we just received exists, by first reading the file entered,  we do that in the try block, because we might not find the file.
+
+		FileReader fileRead = new FileReader(fileNameEnteredByUser); //Here We are reading the file entered by the user;
+		
+		fileAvailableInTheDirectory = new Scanner(fileRead); //Here we are scanning (creating a connection between the inputs)
+		
+//5. And if it is available, in order to give the file a proper name (rename it from .txt to .html), we need to find the position of the dot first.
+		
+		dotIndex = fileNameEnteredByUser.lastIndexOf(".");
+		
+		//if the position of the dot was not found, it means there was no dot in the entered file name or path, so we must add the dot and the html
+		
+		if(dotIndex==-1) {
+			
+			fileNameOut	= fileNameEnteredByUser + ".html";
+	
+		//if the position of the dot was found, then we will just cut whatever we have found from the beginning (0) up to where we have found the dot, and then add the .html	
+			
+		}else {
+			
+			fileNameOut	= fileNameEnteredByUser.substring(0, dotIndex) + ".html";
+		}
+		
+		//Now let's print out the file name we have created
+		
+		filePrintedOut = new PrintWriter(fileNameOut);
+		
+		//6. Inside our try block, let's find out/determine if our file is empty; we will use another try block
+		
+		     //Let's check the first line of the file.
 		
 		try {
-			FileReader myFileReader = new FileReader(fileNameIn);
 			
-			fileIn = new Scanner(myFileReader);
+			line = fileAvailableInTheDirectory.nextLine();
 			
-//Let's get the index of where the . is:, and if there's even a . because we might not have one.
+		}catch(NoSuchElementException e) {
 			
-			dotIndex = fileNameIn.lastIndexOf("."); //Here we are searching for the . and then we will store it within the variable
+			System.out.println("Error: " + e.getMessage());
+	
+		}
+		if(line==null) {
 			
-			if(dotIndex == -1) {       //here we are saying if there was no dot that was found (-1)
+			System.out.println("This file is empty!!!");
 				
-				fileNameOut = fileNameIn + ".html";
+		}else {
+			
+		//7. Now that we found that our file is not empty, let's try to read each line and insert necessary <tags>
+			
+			filePrintedOut.println("<html>");
+			filePrintedOut.println("<head>");
+			filePrintedOut.println("</head>");
+			filePrintedOut.println("<body>");
+			
+			//since we still have the first line stored within the variable line, let's add that before we move
+			
+			filePrintedOut.println(line);
+			
+			//now let's create a while loop that is going to continuously read our file until we finish
+			
+			while(fileAvailableInTheDirectory.hasNextLine()) {
+				
+				filePrintedOut.println("<br>");
+				
+			line =	fileAvailableInTheDirectory.nextLine();
+			
+			//Now, for some reason, if the line above is empty, we want to print a <br> tag
+			
+			if(line.isEmpty()) {
+				
+				filePrintedOut.println("<br>");
+				
+			}else {
+				
+				filePrintedOut.println(line);
 				
 			}
-			else {
-				fileNameOut = fileNameIn.substring(0, dotIndex) + ".html";
+				
 			}
 			
-			fileOut = new PrintWriter(fileNameOut);
+			filePrintedOut.println("</body>");
+			filePrintedOut.println("<html>");
 			
-		} catch (FileNotFoundException e) {
-			System.out.println("File was not found!!!");
-}
-
-
+			//For fun, let's let the user know that the file is finished processing/being written
+			
+			System.out.println("HTML File is processed!!!");
+				
+		}
 		
-//Determine if file is empty
+		myScanner.close();
+
+		fileAvailableInTheDirectory.close();
+		
+	} catch (FileNotFoundException e) {
+		
+		System.out.println("Unfortunately, no file was found!!!!");
+		
+	}	
+	
 //		5. I will read each line and insert necessary html <tags>
+	
+	
+	
 	}
 
 }
